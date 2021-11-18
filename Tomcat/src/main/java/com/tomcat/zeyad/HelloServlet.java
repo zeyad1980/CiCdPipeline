5929c5f742f733a62267eb6089e1b2130fbdb51a
ziad ziad <zdn_80@yahoo.com>
2021-11-18 12:24:54 -0500

    CICDPipeline

---
+++ Tomcat/src/main/java/com/tomcat/zeyad/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.tomcat.zeyad;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet("/hello")
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setHeader("Content-Type", "text/html");
+        resp.setStatus(200);
+        resp.getOutputStream().println("<h1>Hello World from a deployed wer file!</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
